58df00e83d9bb56138aa33adf39087d188f0a926
Denis <Nolane16@gmail.com>
2017-09-06 00:37:10 +0300

    Initial commit

---
+++ lesson-1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace lesson_1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.graphPictureBox = new System.Windows.Forms.PictureBox();
+            this.functionComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.viewButton = new System.Windows.Forms.Button();
+            this.leftTextBox = new System.Windows.Forms.TextBox();
+            this.rightTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.graphPictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // graphPictureBox
+            // 
+            this.graphPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.graphPictureBox.Name = "graphPictureBox";
+            this.graphPictureBox.Size = new System.Drawing.Size(337, 276);
+            this.graphPictureBox.TabIndex = 0;
+            this.graphPictureBox.TabStop = false;
+            // 
+            // functionComboBox
+            // 
+            this.functionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.functionComboBox.FormattingEnabled = true;
+            this.functionComboBox.Location = new System.Drawing.Point(355, 13);
+            this.functionComboBox.Name = "functionComboBox";
+            this.functionComboBox.Size = new System.Drawing.Size(157, 21);
+            this.functionComboBox.TabIndex = 1;
+            this.functionComboBox.SelectedIndexChanged += new System.EventHandler(this.functionComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "От";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "До";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.viewButton);
+            this.panel1.Controls.Add(this.leftTextBox);
+            this.panel1.Controls.Add(this.rightTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(355, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(157, 248);
+            this.panel1.TabIndex = 7;
+            // 
+            // viewButton
+            // 
+            this.viewButton.Location = new System.Drawing.Point(48, 65);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(100, 23);
+            this.viewButton.TabIndex = 4;
+            this.viewButton.Text = "Обновить";
+            this.viewButton.UseVisualStyleBackColor = true;
+            this.viewButton.Click += new System.EventHandler(this.viewButton_Click);
+            // 
+            // leftTextBox
+            // 
+            this.leftTextBox.Location = new System.Drawing.Point(48, 7);
+            this.leftTextBox.Name = "leftTextBox";
+            this.leftTextBox.Size = new System.Drawing.Size(100, 20);
+            this.leftTextBox.TabIndex = 2;
+            // 
+            // rightTextBox
+            // 
+            this.rightTextBox.Location = new System.Drawing.Point(48, 39);
+            this.rightTextBox.Name = "rightTextBox";
+            this.rightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.rightTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 300);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.functionComboBox);
+            this.Controls.Add(this.graphPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Графопостроитель";
+            ((System.ComponentModel.ISupportInitialize)(this.graphPictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox graphPictureBox;
+		private System.Windows.Forms.ComboBox functionComboBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button viewButton;
+        private System.Windows.Forms.TextBox leftTextBox;
+        private System.Windows.Forms.TextBox rightTextBox;
+    }
+}
+
---
+++ lesson-1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_1
+{
+	public partial class Form1 : Form
+	{
+		private static int STEPS = 100;
+
+		private Graphics g;
+
+        private float leftLimit = -1;
+        private float rightLimit = 1;
+        private Func<float, float> func;
+
+        private static Dictionary<String, Func<float, float>> options = new Dictionary<string, Func<float, float>>();
+
+        static Form1()
+        {
+            options["sin(x)"] = x => (float)Math.Sin(x);
+            options["x^2"] = x => x * x;
+            options["abs(x)"] = x => Math.Abs(x);
+            options["sin(x) * cos(2/x)"] = x => (float)(Math.Sin(x) * Math.Cos(2 / x));
+            options["tg(x)"] = x => (float)(Math.Tan(x));
+            options["tgh(x)"] = x => (float)(Math.Tanh(x));
+        }
+
+        public Form1()
+		{
+			InitializeComponent();
+
+            leftTextBox.Text = leftLimit.ToString();
+            rightTextBox.Text = rightLimit.ToString();
+            functionComboBox.Items.AddRange(options.Keys.ToArray());
+			graphPictureBox.Image = new Bitmap(graphPictureBox.Width, graphPictureBox.Height);
+			g = Graphics.FromImage(graphPictureBox.Image);
+		}
+
+        private void RedrawGraph()
+        {
+            PointF[] points = new PointF[STEPS + 1];
+            float min = float.MaxValue, max = float.MinValue;
+            for (int i = 0; i <= STEPS; ++i)
+            {
+                float x = leftLimit + i * (rightLimit - leftLimit) / STEPS;
+                float y = func(x);
+                points[i].X = x;
+                points[i].Y = y;
+                if (y < min) min = y;
+                if (max < y) max = y;
+            }
+            if (min == max) max = min + 1;
+            for (int i = 0; i <= STEPS; ++i)
+            {
+                points[i].X = (points[i].X - leftLimit) / (rightLimit - leftLimit) * graphPictureBox.Width;
+                points[i].Y = (max - points[i].Y) / (max - min) * graphPictureBox.Height;
+            }
+            g.Clear(Color.White);
+            g.DrawLines(Pens.Black, points);
+            graphPictureBox.Invalidate();
+        }
+
+		private void functionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            func = options[(string)comboBox.SelectedItem];
+            RedrawGraph();
+        }
+
+        private void viewButton_Click(object sender, EventArgs e)
+        {
+            float new_left;
+            float new_right;
+            try
+            {
+                new_left = float.Parse(leftTextBox.Text);
+                new_right = float.Parse(rightTextBox.Text);
+                if (new_right <= new_left)
+                {
+                    MessageBox.Show("\"От\" должно быть меньше чем \"До\".");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("В поле ввода должно быть число.");
+                return;
+            }
+            leftLimit = new_left;
+            rightLimit = new_right;
+            RedrawGraph();
+        }
+    }
+}
